9baf91573a3502881b70b3888266062dacd4ecf4
Gg <58531623+djghis@users.noreply.github.com>
2021-03-11 15:35:48 +0000

    checkoutProduct description and image added, delete prodicut form basket funtion working

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,9 +1,13 @@
-import React from 'react'
-import './Checkout.css'
-import Subtotal from './Subtotal'
+import React from 'react';
+import './Checkout.css';
+import { useStateValue } from './StateProvider';
+import Subtotal from './Subtotal';
+import CheckoutProduct from './CheckoutProduct';
 
 
 function Checkout() {
+    const [{basket}] = useStateValue();
+    
     return (
         <div className="checkout">
             <div className="checkout__left" >
@@ -13,6 +17,19 @@ function Checkout() {
 
                 <div>
                     <h2 className="checkout__title">Your shopping basket</h2>
+
+                    {basket.map(item => (
+                        <CheckoutProduct
+                            key={item.id}
+                            id={item.id}
+                            title={item.title}
+                            image={item.image}
+                            price={item.price}
+                            rating={item.rating}
+                         />
+                    ))}
+
+                    
                 </div>
 
             </div>
--- src/Reducer.js
+++ src/Reducer.js
@@ -18,6 +18,23 @@ const reducer = (state, action) => {
                 basket: [...state.basket, action.item]
             };
 
+        case 'REMOVE_FROM_BASKET':
+            // this is there if an item keep the same id when added multiple times in the basket.
+            const index = state.basket.findIndex(
+                (basketItem) => basketItem.id === action.id
+            );
+            let newBasket = [...state.basket];
+
+            if (index >= 0) {
+                newBasket.splice(index, 1);
+            } else {
+                console.warn(`cant remove item with product id: ${action.id} as I can;t find it in the basket.`)
+            }
+
+            return {...state,
+            basket: newBasket
+        }
+
         default:
             return state;
     }
